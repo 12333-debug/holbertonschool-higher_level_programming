7df4b82f8207878e74720472de69194aac511cba
lonie <12333@holbertonstudents.com>
2026-06-30 15:27:48 +0200

    change the code

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (argument >= 1) {
+const count = process.argv.length - 2;
+if (count === 0) {
+  console.log('No argument');
+} else if (count === 1) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
